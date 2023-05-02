535b546804bee255fa44d7e1d436365dc33ee1b5
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-05-02 14:07:57 +0300

    Update files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,23 @@
 import { Component } from "react";
 import { Container } from "./App.styled";
-import { Form } from "./Form/Form";
+import { Form } from "../Form/Form";
 
 export class App extends Component {
 
-state = {
-
-}
 
 formSubmitHandler = data => {
   console.log(data);
 }
 
+
 render() {
+
     return (
       <Container>
         <Form onSubmit={this.formSubmitHandler} />
       </Container>
+
+
 )
 
 }
