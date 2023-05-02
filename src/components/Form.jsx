5e2c5944f48cba6ec742794702a7e2f42bc9f416
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-05-02 12:42:43 +0300

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,6 +7,8 @@ export class Form extends Component {
     state = {
         name: '',
         tag: '',
+        experience: 'junior',
+        licence: false,
     }
 
     nameInputId = nanoid();
@@ -32,6 +34,12 @@ export class Form extends Component {
             tag: '',
         })
     }   
+
+    handleLicenceChange = e => {
+      console.log(e.currentTarget.checked);
+
+      this.setState({licence: e.currentTarget.checked})
+    }
       
 
     render() {
@@ -59,9 +67,50 @@ export class Form extends Component {
                       id={this.tagInputId}
                   />
           </label>
+
+          <p>Ваш уровень разработчика:</p>
       
+          <label>
+          <input 
+              type="radio" 
+              name='experience' 
+              value='junior' 
+              onChange={this.handleChange}
+              checked={this.state.experience === 'junior'} /> 
+              Junior
+          </label>
+
+          <label>
+          <input 
+              type="radio" 
+              name='experience' 
+              value='middle' 
+              onChange={this.handleChange}
+              checked={this.state.experience === 'middle'} /> 
+              Middle
+          </label>
+
+          <label>
+          <input 
+              type="radio" 
+              name='experience' 
+              value='senior' 
+              onChange={this.handleChange}
+              checked={this.state.experience === 'senior'} /> 
+              Senior
+          </label>
+
+          <label>
+          <input 
+              type="checkbox" 
+              name="licence" 
+              checked={this.state.licence}
+              onChange={this.handleLicenceChange}
+           /> 
+           Согласен с условиями
+          </label>
       
-          <button type="submit">Отправить</button>
+          <button type="submit" disabled={!this.state.licence}>Отправить</button>
            
            </form>
       
